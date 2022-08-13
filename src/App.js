5d4cded897a429355c6ef82c23c0ff93147ab4e0
chenyuf2 <42945468+chenyuf2@users.noreply.github.com>
2022-08-12 23:09:46 -0700

    Clean model files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { Canvas } from "@react-three/fiber";
 import "./App.scss";
 import { Suspense } from "react";
 import { PerspectiveCamera } from "@react-three/drei";
-import Scene from "./component/Scene/Scene";
+import Giraffe from "./component/Giraffe/Giraffe";
 import { NoToneMapping } from "three";
 import Home from "component/Home/Home";
+import Elephant from "./component/Elephant/Elephant";
 
 const App = () => {
   return (
@@ -18,12 +19,13 @@ const App = () => {
         <Suspense fallback={null}>
           <PerspectiveCamera
             makeDefault
-            position={[0, 2, 6]}
+            position={[0, 2, 5.5]}
             near={1}
             far={100}
             fov={60}
           />
-          <Scene />
+          {/* <Giraffe /> */}
+          <Elephant />
         </Suspense>
       </Canvas>
     </>
